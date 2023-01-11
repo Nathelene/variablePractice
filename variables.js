f2cd69a69eaaf61003acddd17c646aa90faf84a6
Natalie Baker <Nathelene76@gmail.com>
2023-01-10 17:23:27 -0700

    Reassign var value

--- variables.js
+++ variables.js
@@ -1,6 +1,9 @@
-var email = ""
-var name = Rory
+var email = "richard@gilmore.com"
+email // => "lorelei@gilmore.com"
 var age = 16
 var birthYear = 1990
 var billsPaid = true
 var ateBreakfast = true 
+console.log(billsPaid)
+console.log(email)
+
